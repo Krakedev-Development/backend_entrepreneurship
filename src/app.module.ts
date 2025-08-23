ba29647f611088f125fee5b341fbaa42599aed45
victorhmartinez <victormartinezg1998@gmail.com>
2025-08-22 22:15:04 -0500

    Implemntacion de servicios de aprendizaje a registros

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 
 import { BusinessModule } from './simulator/bussiness/business.module';
+import { LearningModule } from './simulator/learning/learning.module';
+import { SizeModule } from './simulator/size/size.module';
+import { ModuleModule } from './simulator/module/module.module';
+import { BusinessProgressStepModule } from './simulator/bussinessProgress/business-progress-step.module';
+import { StatusModule } from './simulator/status/status.module';
+import { FinancialRecordModule } from './simulator/financial-record/financial-record.module';
 
 @Module({
   imports: [
 
-   BusinessModule 
+   BusinessModule,
+   LearningModule,
+   SizeModule,
+   ModuleModule,
+   BusinessProgressStepModule,
+   StatusModule,
+   FinancialRecordModule
   ],
   controllers: [],
   providers: [],
--- src/simulator/bussiness/mappers/business.mapper.ts
+++ src/simulator/bussiness/mappers/business.mapper.ts
@@ -5,14 +5,14 @@ import { Business } from '../entities/bussines.entity';
 
 @Injectable()
 export class BusinessMapper {
-  toDomain(prismaBusiness: BusinessPrismaModel): Business {
-     return new Business(
-      prismaBusiness.usuario_id,
-      prismaBusiness.tipo_negocio,
-      prismaBusiness.nombre_negocio,
-      prismaBusiness.ubicacion,
-      prismaBusiness.id_tamano,
-      prismaBusiness.negocio_id,
-    prismaBusiness.fecha_creacion === null ? undefined : prismaBusiness.fecha_creacion    );
-  }
+    toDomain(prismaBusiness: BusinessPrismaModel): Business {
+        return new Business(
+            prismaBusiness.usuario_id,
+            prismaBusiness.tipo_negocio,
+            prismaBusiness.nombre_negocio,
+            prismaBusiness.ubicacion,
+            prismaBusiness.id_tamano,
+            prismaBusiness.negocio_id,
+            prismaBusiness.fecha_creacion === null ? undefined : prismaBusiness.fecha_creacion);
+    }
 }
